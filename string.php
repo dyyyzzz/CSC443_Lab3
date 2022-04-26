96391d21db54e4f22a1351abb72d0229ddf06b77
dyyyzzz <nuradilah688@gmail.com>
2022-04-27 00:22:20 +0800

    commit

--- string.php
+++ string.php
@@ -12,7 +12,7 @@
     $txt2 = "Have a nice day!";
     $greet = $txt1 . " " . $txt2;
     echo $greet;?>
-    echo strlen ("Hello world!");
+    echo (strlen ("Hello world!"));
     echo "<br />";
     echo strpos ("Hello world!","world");
     echo "<br />";
